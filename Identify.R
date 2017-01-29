c5340318f57d5d5a1d9c2cc2a418062aba328dd3
Marcin Łapiński <lapinskm@gmail.com>
2017-01-29 03:45:44 +0100

    Made page identyfication language independent
      * Added naive bayesian model
      * Little improvement in quality and performance

--- Identify.R
+++ Identify.R
@@ -7,66 +7,72 @@ library(compiler)
 
 library(randomForest)
 library(e1071)
+library(class)
 
-tagNames=c('img', 'video', 'audio', 'script', 'style',
-           'article', 'canvas', 'blockquote', 'span',
-           'input', 'form', 'object', 'meta', 'link',
-           #'table', 'h1', 'h2', 'h3', 'li', 'ul', 'tr', 
-           'hr', 'a', 'b', 'p', 'i')
-
-wordCountRegexes=c("password|login|username|użytkownik|zaloguj|hasło",
-                   "forum|post|awatat|avatar|quote",
-                   #"vid|wideo|film",#
-                   #"audio|music|muzyka",#
-                   "zdjęcie|zdjęcia|foto|photo",
-                   "pic|picture|image|obraz",
-                   "artykuł|article",
-#                   "tekst|text",
-                   "portal",
-                   "blog",
-#                   "galeria|galery",
-                   "news|wiadomości|informacje",
-                   "kontakt|contact",
-#                   "buy|kup|cena|koszyk|price",
-#                   "place|miejsce|ulica|lokalizacja",
-#                   "share|udostępnij|prześlij",
-                   "email|e-mail|poczta|sendto",
-                   "bank|ubezpieczen|kredyt|praw",
-                   "bizn|praw|pieni|money|inwest"#,
-                   #"ad|reklama",
-                   #"zdrowie|medycyna|health",
-                   #"uroda|kosmetyki|beauty",
-                   #"play|odtwórz"
-                   )
-
-
-
-extractFeatures<-function(filename)
-{
+readCategory<-function(filename) {
   con <- file(filename, "r")
   descrLines <- readLines(con,n=2)
   
   close(con)
   category   <- strsplit(descrLines[1]," ")[[1]][1]
-  url        <- descrLines[2]
-  #Features are maked by "###" at the end of line for convinience
-  
+  #url        <- descrLines[2]
+  return(category)
+}
+
+#read files and tidy up data
+readDataFile<-function(filename) {
   x <- readChar(filename, file.info(filename)$size)
   
   # remove first two lines containing metadata
-
   x<-stri_replace_first_regex(x,"^.*?\n.*?\n", "" )
-  dirtyPageSize<-stri_length(x)                            ###
-  #html might be malformed
   x<-tidy_html(x)
-  cleanPageSize<-stri_length(x)                            ###
+  return(x)
+}
+
+tagNames=c(#'video', 'audio', 'object','canvas','blockquote',
+           'article', 'span',
+           'input', 'form', 'link',
+          # 'table', 'h1', 'h2', 'h3', 'li', 'ul', 'tr', 'hr',
+          'a' )
+
+wordCountRegexes=c("forum", "blog","video")
+
+extractFeatures<-function(fileContent) {
+  #dirtyPageSize<-stri_length(fileContent)                 ###
+  #names(dirtyPageSize)="dirtyPageSize"
+  #html might be malformed
+
+  cleanPageSize<-stri_length(fileContent)                  ###
+  names(cleanPageSize)="cleanPageSize"
+
   #get tag statistics
-  x_html <- read_html(x)
+  x_html <- read_html(fileContent)
   allNodes <- xml_find_all(x_html,"//*")
   totalTagCount <- length(allNodes)                        ###
+  names(totalTagCount)="totalTagCount"
+
+  titleNodes= xml_find_all(x_html,"//title")
+  titleText =  paste(xml_text(titleNodes))
   
+  metaOgType="NO_OG_TYPE";
+  metaNodes <- xml_find_all(x_html,"//meta")
+  for(i in 1:length(metaNodes) ){
+    nodeAttrs <- unlist(xml_attrs(metaNodes[i]))
+
+    if (!is.na(nodeAttrs["property"])) {
+      if(as.character( nodeAttrs["property"])=="og:type") {
+        if(!is.na(nodeAttrs["content"])) {
+           metaOgType <- nodeAttrs["content"];
+        }
+        break;
+      }
+    }
+  }
+  names(metaOgType)="metaOgType" ###
+
   childrenCountSum <-0
   leaveCount <- 0 #childless nodes                         ###
+
   for(i in 1:totalTagCount[1]) {
     childCount <- length(xml_children(allNodes[i][1]))
     if (childCount != 0) {
@@ -75,101 +81,143 @@ extractFeatures<-function(filename)
       leaveCount <-leaveCount +1;
     }
   }
+  names(leaveCount) <- "leaveCount"
   meanChildCount <- childrenCountSum/totalTagCount         ###
-  
-  tagCounts = laply(tagNames,
-                    function(tagName) {
-                      length(xml_find_all(x_html, paste0("//", tagName)))
-                    }
-                   )
-  
+  names(meanChildCount) <- "meanChildCount"
+
+  tagCounts <- laply(tagNames,
+                     function(tagName) {
+                       length(xml_find_all(x_html, paste0("//", tagName)))
+                     }
+                    )
+  names(tagCounts)<-stri_paste(tagNames, "TagCount", collapse = NULL)
+
+  relTagCounts <- tagCounts/totalTagCount
+  names(relTagCounts)<-stri_paste(tagNames, "RelTagCount", collapse = NULL)
+
   totalTaglessSize <- stri_length(xml_text(x_html))        ###
+  names(totalTaglessSize)<-"totalTaglessSize"
+
   cssNodes <- xml_find_all(x_html, "//style")
   jsNodes <- xml_find_all(x_html, "//script")
   headNodes <- xml_find_all(x_html, "//head")
-  
-  
+
   inlineCssSize <- sum(stri_length(xml_text(cssNodes)))    ###
+  names(inlineCssSize)<-"inlineCssSize"
   inlineJsSize <- sum(stri_length(xml_text(jsNodes)))      ###
+  names(inlineJsSize)<-"inlineJsSize"
   taglessHeadSize <- sum(stri_length(xml_text(headNodes))) ###
-  
-  #remove not displayed directly nodes to continue processing
+  names(taglessHeadSize)<-"taglessHeadSize"
+  imgNodes <- xml_find_all(x_html, "//img")
+  imgTagCount = length(imgNodes)                           ###
+  names(imgTagCount)<-"imgTagCount"   #potential trouble
+
+  imgPaths <- xml_path(imgNodes)
+  imgPaths <- stri_replace_all_regex(str = imgPaths, replacement ="", pattern = "\\[[0-9]*?\\]")
+
+  mostCommonImgPathCount <- table (imgPaths)[1]            ###
+  if(is.na(mostCommonImgPathCount))
+    mostCommonImgPathCount=0
+
+  names(mostCommonImgPathCount) <- "mostCommonImgPathCount"
+
+  #remove not dismostCommonImgPathCountplayed directly nodes to continue processing
   if(length(jsNodes))
      xml_remove(jsNodes)
   if(length(cssNodes))
      xml_remove(cssNodes)
   if(length(headNodes))
     xml_remove(headNodes)
-    
+
   displayedText <- xml_text(x_html)
   displayedTextSize <- stri_length(displayedText)              ###
-  
+  names(displayedTextSize) <- "displayedTextSize"
+
   displToTotalRatio <- displayedTextSize / cleanPageSize       ###
+  names(displToTotalRatio) <- "displToTotalRatio"
   headToTotalRatio <- taglessHeadSize / cleanPageSize          ###
+  names(headToTotalRatio) <- "headToTotalRatio"
 
   wordCounts<-stri_count_regex(displayedText, wordCountRegexes)###
-
-  #ostatecznie formujemy wektor cech                       
-  c(dirtyPageSize,
+  names(wordCounts)<-paste0("wordCount_", (1:length(wordCounts))) # cause troubles
+  #finally we formulate feature vector
+  featureVector=
+    c(#dirtyPageSize,
     cleanPageSize,
     totalTaglessSize,
     inlineCssSize,
     inlineJsSize,
     taglessHeadSize,
+    mostCommonImgPathCount,
+    imgTagCount,
     displayedTextSize,
-    
+
     headToTotalRatio,
     displToTotalRatio,
-    
-    #tagCounts,
-    tagCounts/totalTagCount, # relative tag counts
+
+    tagCounts,
+#    relTagCounts, # relative tag counts
     totalTagCount,
     meanChildCount,
     leaveCount,
-    
     wordCounts,
-    category);
+    metaOgType);
 }
+
+#compile function to speed up execution
 extractFeatures_ <- cmpfun(extractFeatures)
 
 #read list of files
-
 filenames <- paste0("train/",list.files("train/"))
 
-#extact feature vectors from raw data
-data = ldply(.data =  filenames, .fun = extractFeatures_)
+#read files content
+categories <- ldply( .data =  filenames, .fun = readCategory )
+categories <- as.factor(unlist(categories))
+
+filesContent <- ldply( .data =  filenames, .fun = readDataFile )
+
+#extact feature vectors from file content
+featureData <- ldply(.data =  unlist (filesContent), .fun = extractFeatures_)
+featureData$.id <- NULL
+featureData["metaOgType"] <- as.factor(unlist(featureData["metaOgType"]))
+
+#Data size
+dataSize <- nrow(featureData)
+vectSize <- ncol(featureData)
 
 #Split data to learning and test sets
-dataSize <- nrow(data)
-vectSize <- ncol(data)
+testIdx  <- sample(dataSize,dataSize * 0.1)
 
-testIdx  <- sample(dataSize,dataSize * 0.3)
-test <- data[testIdx, ]
-learn <- data[-testIdx, ]
+xlearn <- featureData[-testIdx,]
+xtest  <- featureData[ testIdx,]
 
+ylearn <- categories[-testIdx]
+ytest  <- categories[ testIdx]
 
-#dane
-x <- learn[, 1:vectSize-1]
-y <- as.factor(learn[, vectSize])
+#Some models needs to be feed by matrix
+xlearn_matrix <- data.matrix(xlearn)
+xtest_matrix  <- data.matrix(xtest )
 
-xtest <- test [, 1:vectSize-1]
-ytest <- as.factor(test [, vectSize])
+# try SVM model
+svm_model  <- svm(x=xlearn_matrix, y=ylearn)
+ytest_pred  <- predict(svm_model, xtest_matrix)
+ylearn_pred <- predict(svm_model, xlearn_matrix)
+# compare result of prediction for learn and test data
+sum(rep(1,length(ylearn_pred)) [ylearn_pred == ylearn])/length(ylearn_pred)
+sum(rep(1,length(ytest_pred))  [ytest_pred  == ytest] )/length(ytest_pred)
 
-x_matrix     <- data.matrix(x)
-xtest_matrix <- data.matrix(xtest)
+#try naive bayes model
+bayes_model <- naiveBayes(x=xlearn_matrix, y=ylearn)
 
+ytest_pred  <- predict(bayes_model, xtest_matrix)
+ylearn_pred <- predict(bayes_model, xlearn_matrix)
+# compare result of prediction for learn and test data
+sum(rep(1,length(ylearn_pred)) [ylearn_pred == ylearn])/length(ylearn_pred)
+sum(rep(1,length(ytest_pred))  [ytest_pred  == ytest] )/length(ytest_pred)
 
-#svm_model <- svm(x=x_matrix, y=y, gamma = 0.01)
-#ytest_pred    <- predict(svm_model, xtest_matrix)
-#y_pred   <- predict(svm_model, x_matrix)
-
-#sum(rep(1,length(y_pred)) [y_pred == y])/length(y_pred)
-#sum(rep(1,length(ytest_pred)) [ytest_pred == ytest])/length(ytest_pred)
-
-#Teach forest
-forest = randomForest(x, y, xtest, ytest, ntree=2000, keep.forest = TRUE)
+#try random forest # (still the best we have)
+forest = randomForest(xlearn, ylearn, xtest, ytest, keep.forest = TRUE)
 forest 
 
-#jakie zmienne były istotne
+#which variables were revelant
 forest$importance
-failed = filenames [data[, vectSize] == forest$predicted[filenames] ] 
